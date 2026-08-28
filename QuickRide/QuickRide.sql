50ce4fa99bf2f73694bdad031258c44bad80f2a0
clet001 <nwaforclement7@gmail.com>
2026-08-28 07:57:09 +0100

    Quick Ride

---
+++ QuickRide/QuickRide.sql
@@ -0,0 +1,74 @@
+USE quickride;
+
+-- Q1
+SELECT trip_id, rider_name, fare, city 
+FROM quickride.trips 
+WHERE city = 'Lagos';
+
+-- Q2
+SELECT rider_name, city, fare 
+FROM trips 
+ORDER BY fare 
+DESC LIMIT 5;
+
+-- Q3
+SELECT DISTINCT city 
+FROM trips;
+
+-- Q4
+SELECT * 
+FROM quickride.trips 
+WHERE payment_method = 'Card' 
+and fare > 5000; 
+
+-- Q5
+SELECT * 
+FROM trips 
+WHERE distance_km 
+BETWEEN 5 AND 10;
+
+
+-- Q6
+SELECT * 
+FROM quickride.trips 
+WHERE rider_name LIKE 'A%';
+
+-- Q7
+SELECT * 
+FROM quickride.trips 
+WHERE payment_method 
+IN ('Card', 'Wallet');
+
+-- Q8
+SELECT * 
+FROM quickride.trips 
+WHERE rating IS NULL;
+
+-- Q9
+SELECT city, max(fare) as highest_fare 
+FROM quickride.trips 
+WHERE status = 'Completed' 
+GROUP BY city ORDER BY highest_fare DESC;
+
+-- Q10
+
+SELECT *
+FROM trips 
+WHERE status = 'Cancelled' 
+;
+
+-- Q11
+SELECT 
+    MAX(fare),
+	MIN(fare)
+FROM trips
+WHERE status = 'Completed';
+
+-- Q12
+SELECT 
+	vehicle_type,
+	COUNT(*)
+FROM trips
+WHERE status = 'Completed'
+GROUP BY vehicle_type;
+
